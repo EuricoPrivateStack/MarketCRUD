27d95bb027f5756cdd599c3bff1e8e94a6ec87d8
Eurico149 <euricogabriel149@gmail.com>
2025-06-07 19:41:45 -0300

    Melhorando docker-compose e apagando testeNginx desnecessario

--- api/testeNginx/semNginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:3000/auth/register \
-  -H "Content-Type: application/json" \
-  -d '{"username":"eur149","password":"123456"}' &
-curl -X POST http://localhost:3000/auth/register \
-  -H "Content-Type: application/json" \
-  -d '{"username":"eurico","password":"123456"}'
